715727501d297dcd0577b6ce6054851c0c41a182
obada <obadaquraan@yahoo.com>
2019-10-25 11:27:14 +0200

    initial fix

--- js/app.js
+++ js/app.js
@@ -8,15 +8,19 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+this.items = new Cart(product,quantity);
+
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('productKey',JSON.stringify(this.items));
 };
 
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function(itemIndex) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+this.items.slice(itemIndex,1)
 };
 
 var CartItem = function(product, quantity) {
